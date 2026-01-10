f1644e8d75a1b9d28accdea0d7115ca716e5d473
mauricio <mau.movilla@gmail.com>
2026-01-09 19:54:18 -0500

    a

--- Assets/Scripts/Server/Bootstrap/ServerBootstrap.cs
+++ Assets/Scripts/Server/Bootstrap/ServerBootstrap.cs
@@ -6,12 +6,14 @@ namespace Server.Bootstrap
 {
     public class ServerBootstrap : MonoBehaviour
     {
+        public GameObject ServerPrefab;
         void Start()
         {
             Debug.Log( "[SERVER] ServerBootstrap started" );
             Debug.Log( "Application.isBatchMode = " + Application.isBatchMode );
             NetworkManager.Singleton.StartServer();
             Debug.Log( "[SERVER] Servidor dedicado iniciado" );
+            Instantiate( ServerPrefab );
         }
     }
 }
